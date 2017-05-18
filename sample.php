be8a90c9472887430933296dcb4c3171a012d7f0
pandiya110 <pandiya110@gmail.com>
2017-05-18 18:03:19 +0530

    files modifie

--- sample.php
+++ sample.php
@@ -1,4 +1,10 @@
 <?php
-echo 'Hello World';
-echo 'This is sample PHP files';
+$address = "3428 Simpson Ave Ocean City NJ";
+$formattedAddr = str_replace(' ','+',$address);
+$geocodeFromAddr = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.$formattedAddr.'&sensor=false'); 
+$output = json_decode($geocodeFromAddr);
+$data['latitude']  = $output->results[0]->geometry->location->lat; 
+$data['longitude'] = $output->results[0]->geometry->location->lng;
+
+print_r($data);
 ?>
